35d4c4eb19dd24ef2d2c807dd78ce01e75a7a615
HKP <kimphat.devcs@gmail.com>
2021-09-11 16:53:18 +0700

    Update FrontendLayout

---
+++ solarcofffee.frontend/src/components/SideMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="side-menu-container">
+    <img
+      id="imgLogo"
+      alt="Solar Coffee Logo"
+      src="../assets/images/SolarCoffeeLogo.png"
+    />
+    <h1>Management Portal</h1>
+    <solar-button
+      id="menuInventory"
+      :link="'/inventory'"
+      isFullwidth
+    >
+      Inventory
+    </solar-button>
+    <solar-button id="menuCustomer" :link="'/customer'" isFullwidth>
+      Manage Customers
+    </solar-button>
+    <solar-button id="menuInvoice" :link="'/invoice/new'" isFullwidth>
+      New Invoice
+    </solar-button>
+    <solar-button id="menuOrders" :link="'/order'" isFullwidth>
+      Orders
+    </solar-button>
+  </div>
+</template>
+
+<script lang="ts">
+import SolarButton from "./SolarButton.vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "SideMenu",
+  components: { SolarButton },
+})
+export default class SideMenu extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "../scss/global.scss";
+.side-menu-container {
+  background-color: #fcfcfc;
+  height: 100vh;
+  width: $menu-width;
+  display: flex;
+  flex-direction: column;
+  padding: 0.8rem;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 1px 1px rgba(0, 0, 0, 0.24);
+  box-sizing: border-box;
+}
+
+#imgLogo {
+  width: 100%;
+}
+h1 {
+  font-size: 1.2rem;
+  margin: 1rem 0;
+}
+</style>
---
+++ solarcofffee.frontend/src/components/SolarButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="btn-link">
+    <button
+      @click="visitRoute"
+      :class="['solar-button', { fullWidth: isFullwidth }]"
+    >
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "SolarButton",
+  components: {},
+})
+export default class SolarButton extends Vue {
+  @Prop({ required: false, type: String }) link?: string;
+  @Prop({ required: false, type: Boolean, default: false })
+  isFullwidth?: boolean;
+
+  visitRoute() {
+    this.$router.push({ path: this.link });
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../scss/global.scss";
+.solar-button {
+  background: lighten($solar-blue, 10%);
+  color: white;
+  padding: 0.8rem;
+  transition: background-color 0.5s;
+  margin: 0.3rem 0.2rem;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1rem;
+  min-width: 100px;
+  border: none;
+  border-bottom: 2px solid darken($solar-blue, 20%);
+  border-radius: 3px;
+  &:hover {
+    background: lighten($solar-blue, 20%);
+    transition: background-color 0.5s;
+  }
+  &:disabled {
+    background: lighten($solar-blue, 15%);
+    border-bottom: 2px solid lighten($solar-blue, 20%);
+  }
+  &:active {
+    background: $solar-blue;
+    border-bottom: 2px solid lighten($solar-blue, 20%);
+  }
+}
+.fullWidth {
+  display: block;
+  width: 100%;
+}
+</style>
