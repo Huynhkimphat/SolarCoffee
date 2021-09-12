c8ff01ec42648041f60d57a54de46620733d4f70
HKP <kimphat.devcs@gmail.com>
2021-09-12 13:16:08 +0700

    Update

--- solarcofffee.frontend/src/components/modals/SolarModal.vue
+++ solarcofffee.frontend/src/components/modals/SolarModal.vue
@@ -82,13 +82,13 @@ export default class SolarModal extends Vue {
 }
 
 .modal-fade-enter,
-.modal-fade-leave-enter {
+.modal-fade-leave-active {
   opacity: 0;
 }
 
 .modal-fade-enter-active,
 .modal-fade-leave-active {
-  transition: opacity 0.5s ease;
+  transition: opacity .5s ease;
 }
 
 </style>
